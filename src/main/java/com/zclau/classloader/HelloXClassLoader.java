f35df9d5e8fc05f058fbe5fbcd2a31edec9e7f31
zclau <349856491@qq.com>
2021-08-06 01:21:29 +0800

    自定义一个 Classloader，加载一个 Hello.xlass 文件，执行 hello 方法，此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件

---
+++ src/main/java/com/zclau/classloader/HelloXClassLoader.java
@@ -0,0 +1,40 @@
+package com.zclau.classloader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.Base64;
+
+public class HelloXClassLoader extends ClassLoader{
+
+    public static void main(String[] args) throws Exception {
+        Class<?> clazz = new HelloXClassLoader().loadClass("Hello");
+        Object instance = clazz.newInstance();
+        // 调用实例方法
+        Method method = clazz.getMethod("hello");
+        method.invoke(instance);
+    }
+
+
+    @Override
+    protected Class<?> findClass(final String name) throws ClassNotFoundException {
+        try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(name + ".xlass")){
+            int length = is.available();
+            byte[] bytes = new byte[length];
+            is.read(bytes);
+            byte[] decodeBytes = decode(bytes);
+            return defineClass(name, decodeBytes, 0, bytes.length);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            throw new ClassNotFoundException(name, ioe);
+        }
+    }
+
+    public byte[] decode(byte[] bytes) {
+        byte[] decodeBytes = new byte[bytes.length];
+        for (int i = 0; i < bytes.length; i++) {
+            decodeBytes[i] = (byte) (255 - bytes[i]);
+        }
+        return decodeBytes;
+    }
+}
