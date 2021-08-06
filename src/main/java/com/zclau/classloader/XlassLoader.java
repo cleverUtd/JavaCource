af2f2eeaa0280f3b27366baebcdd63eb19127902
zclau <349856491@qq.com>
2021-08-07 01:03:42 +0800

    第一周作业

--- src/main/java/com/zclau/classloader/XlassLoader.java
+++ src/main/java/com/zclau/classloader/XlassLoader.java
@@ -4,20 +4,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
 
-public class HelloXClassLoader extends ClassLoader{
+public class XlassLoader extends ClassLoader{
 
     public static void main(String[] args) throws Exception {
-        Class<?> clazz = new HelloXClassLoader().loadClass("Hello");
+        String className = "Hello";
+        Class<?> clazz = new XlassLoader().findClass(className);
         Object instance = clazz.newInstance();
         // 调用实例方法
         Method method = clazz.getMethod("hello");
         method.invoke(instance);
     }
 
-
     @Override
     protected Class<?> findClass(final String name) throws ClassNotFoundException {
-        try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(name + ".xlass")){
+        String suffix = ".xlass";
+        try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(name + suffix)){
             int length = is.available();
             byte[] bytes = new byte[length];
             is.read(bytes);
