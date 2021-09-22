390baf91df6414b6465f9eb2c9285f0663481382
zclau <349856491@qq.com>
2021-09-23 01:13:46 +0800

    week7

--- week07/src/main/java/db_insert/BatchInsertTest.java
+++ week07/src/main/java/db_insert/BatchInsertTest.java
@@ -3,29 +3,65 @@ package db_insert;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.concurrent.CountDownLatch;
 
 public class BatchInsertTest {
 
-    public static void main(String[] args) {
-        Connection connection = JdbcUtil.getConnection();
-
+    private static void singleThread() {
         long start = System.currentTimeMillis();
-        try (PreparedStatement ps = connection.prepareStatement("INSERT INTO t_order (order_seq, user_id) VALUES (?,?)")) {
+        bulkInsert(1, 1);
+        System.out.println("costs: " + (System.currentTimeMillis() - start));
+    }
 
-            for (int i = 0; i < 1000000; i++) {
+    private static void multiThread() throws InterruptedException {
+        long start = System.currentTimeMillis();
+
+        CountDownLatch cdt = new CountDownLatch(2);
+        Thread run1 = new Thread(() -> {
+            bulkInsert(1, 2);
+            cdt.countDown();
+        });
+        Thread run2 = new Thread(() -> {
+            bulkInsert(0, 2);
+            cdt.countDown();
+        });
+
+
+        run1.start();
+        run2.start();
+
+        cdt.await();
+        System.out.println("costs: " + (System.currentTimeMillis() - start));
+
+    }
+
+    private static void bulkInsert(int s, int step) {
+        Connection connection = JdbcUtil.getConnection();
+        PreparedStatement ps = null;
+
+        try  {
+            ps = connection.prepareStatement("INSERT INTO t_order (order_seq, user_id) VALUES (?,?)");
+            connection.setAutoCommit(false);
+            for (int i = s; i < 1000000; i+=step) {
                 ps.setLong(1, i);
                 ps.setLong(2, i);
                 ps.addBatch();
 
-                if (i % 1000 == 0) {
+                if (i % 3000 == 0) {
                     ps.executeBatch();
                 }
             }
-            System.out.println("costs: " + (System.currentTimeMillis() - start));
+            ps.executeBatch();
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            JdbcUtil.releaseDB(connection, null, null);
+            JdbcUtil.releaseDB(connection, ps, null);
         }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+//        singleThread();
+        multiThread();
     }
 }
